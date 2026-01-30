cda3529958788b83cda381b0f30801322963eaf2
Ravi Jagtap <ravijagtap313@gmail.com>
2026-01-30 18:14:25 +0000

    learn the conversions operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -4,3 +4,24 @@ console.log(typeof score)
 
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber)
+
+let isLoggedIN =1
+
+let booleanIsLoggedIn=Boolean(isLoggedIN)
+console.log(booleanIsLoggedIn);
+
+                              //*************Opreations***************//
+let str1="Ravi"
+let str2=" Jagtap"
+
+let str3=str1+str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+"2"+3);
+console.log(1+2+"3");
+
+
+
