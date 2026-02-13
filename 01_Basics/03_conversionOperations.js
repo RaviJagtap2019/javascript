e545c1a298dd55a6fffbbb852971be3781ddd7c3
Ravijagtap2019 <ravijagtap313@gmail.com>
2026-02-13 10:32:10 +0530

    Add new file or add some more conversions operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,27 +1,47 @@
 let score =33
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 let isLoggedIN =1
 
 let booleanIsLoggedIn=Boolean(isLoggedIN)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+let userName="Ravi jagtap"
+
+let string=Number(userName)
+console.log(string)
+console.log(typeof string);
+
+
+let marks="33"
+console.log(typeof marks);
+
+let valueInMarks=Number(marks)
+console.log(marks)
+
+console.log(typeof valueInMarks);
+
+
+
+
 
                               //*************Opreations***************//
 let str1="Ravi"
 let str2=" Jagtap"
 
 let str3=str1+str2
-console.log(str3);
+/*console.log(str3);
 
 
 console.log("1"+2);
 console.log(1+"2");
 console.log(1+"2"+3);
-console.log(1+2+"3");
+console.log(1+2+"3");*/
 
 
 
